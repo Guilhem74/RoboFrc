00733ab05a6b1b4de9ef7a0aaf4d10d6b0bef38d
Guilhem AZZANO <g.azzano74@gmail.com>
2017-02-23 22:48:19 +0100

    Modification
    Ce code minimal permet de tester l'ensemble des moteurs et des
    actionneurs.
    Chaque verrin est mappé sur une touche du joystick, ils sont controlés
    par une seule touche pour les deux états possible.
    La base roulante est pilotable que ca soit mecanum ou tank

--- 5553_2017/src/BaseRoulante.h
+++
@@ -1,17 +0,0 @@
-/*
- * BaseRoulante.h
- *
- *  Created on: 27 déc. 2016
- *      Author: REBAUDET Thomas
- */
-#include "WPILib.h"#ifndef SRC_BASEROULANTE_H_
-#define SRC_BASEROULANTE_H_
-
-class BaseRoulante {
-public:	BaseRoulante();
-	virtual ~BaseRoulante();	void setRobotMode(int);	int getRobotMode();
-	void mvtJoystick(Joystick*, ADXRS450_Gyro* );	void deposeRoueAuto(Joystick* , ADXRS450_Gyro*, Ultrasonic*,Ultrasonic*);	void resetModeAuto();	void MonterCorde(Joystick* Joystick1);    VictorSP mecaFrontLeft;	VictorSP mecaFrontRight;	VictorSP mecaBackRight;	VictorSP mecaBackLeft;	VictorSP rouleau;	RobotDrive* R2D2;	DoubleSolenoid verins_BASE;	double approach_speed;	double align_dist;	double align_marge;	double rot_speed;	double rot_marge;
-private:	int robotMode;
-	float zCoeff = 0.5;	int mode_auto;
-};
-#endif /* SRC_BASEROULANTE_H_ */
--- 5553_2017/src/Robot.cpp
+++ 5553_2017/src/Robot.cpp
@@ -6,16 +6,11 @@
 
 #include "WPILib.h"
 
-#include <Encoder.h>
 
 
 
 
-#include <thread>
-#include <CameraServer.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/core/types.hpp>
+
 
 
 
@@ -33,13 +28,23 @@ public:
 	DoubleSolenoid* verins_2;
 	DoubleSolenoid* verins_3;
 	DoubleSolenoid* verins_4;
-
+	Servo* Sai;
+	AnalogInput *Ai;
+	int etat1;
+	int etat2;
+	int etat3;
+	int etat4;
 	//bidon
 
 	void RobotInit() {
+		Ai= new AnalogInput(1);//Fin de course
 
 		// initialisation des objets et données
-
+		Sai =new Servo(5);//Servo moteur
+		 etat1=1;
+		 etat2=1;
+		 etat3=1;
+		 etat4=1;
 		Joystick1 = new Joystick(0);								// à connecter sur port USB0
 		M1= new VictorSP(0);
 		M2= new VictorSP(1);
@@ -50,7 +55,6 @@ public:
 		verins_2= new DoubleSolenoid(2,3);
 		verins_3= new DoubleSolenoid(4,5);
 		verins_4= new DoubleSolenoid(6,7);
-
 	}
 
 	void AutonomousInit() override {
@@ -66,50 +70,102 @@ public:
 	}
 
 	void TeleopPeriodic() {
-/*M1= new VictorSP(0);
-		M2= new VictorSP(1);
-		M3= new VictorSP(2);
-		M4= new VictorSP(3);
-		M5= new VictorSP(4);*/
-		M1->Set(0.9);
-		if(Joystick1->GetRawButton(1)){
-			M1->Set(0.6);
-			M2->Set(0.6);
-			M3->Set(0.6);
-			M4->Set(0.6);
-
+		Sai->SetAngle(-20);
+		float x= -Joystick1->GetX();
+		float y= -Joystick1->GetY();
+		float z= -Joystick1->GetZ();
+		if(etat1==0)
+		{
+			M1->Set(-y+0.5*z);
+			M2->Set(-y+0.5*z);
+			M3->Set(y+0.5*z);
+			M4->Set(y+0.5*z);
 		}
 		else
 		{
-			M1->Set(0);
-			M2->Set(0);
-			M3->Set(0);
-			M4->Set(0);
-
-		}
-		if(Joystick1->GetRawButton(2)){
-			M4->Set(0.9);
-		}
-		else
-		{
-			M4->Set(0);
-		}
-		if(Joystick1->GetRawButton(3)){
-			verins_1->Set(frc::DoubleSolenoid::kForward);
-			verins_2->Set(frc::DoubleSolenoid::kForward);
-			verins_3->Set(frc::DoubleSolenoid::kForward);
-			verins_4->Set(frc::DoubleSolenoid::kForward);
-
-
-
-		}
-		if(Joystick1->GetRawButton(4)){
-			verins_1->Set(frc::DoubleSolenoid::kReverse);
-			verins_2->Set(frc::DoubleSolenoid::kReverse);
-			verins_3->Set(frc::DoubleSolenoid::kReverse);
-			verins_4->Set(frc::DoubleSolenoid::kReverse);
+			M1->Set(-y-x+0.5*z);
+			M2->Set(-y+x+0.5*z);
+			M3->Set(x+y+0.5*z);
+			M4->Set(-x+y+0.5*z);
 		}
 
+
+		if(Joystick1->GetRawButton(3)){//Mecanum
+			etat1=1-etat1;
+			if(etat1==0)
+				verins_1->Set(frc::DoubleSolenoid::kForward);
+			else
+				verins_1->Set(frc::DoubleSolenoid::kReverse);
+
+			std::cout<<"Verin1"<<std::endl;
+			std::cout<<etat1<<std::endl;
+			Wait(1);
+		}
+		if(Joystick1->GetRawButton(4)){//Lever pince
+			etat2=1-etat2;
+			if(etat2==0)
+			{
+				verins_2->Set(frc::DoubleSolenoid::kForward);
+
+			}
+			else
+			{
+				verins_2->Set(frc::DoubleSolenoid::kReverse);
+
+			}
+
+			std::cout<<"Verin2"<<std::endl;
+			std::cout<<etat2<<std::endl;
+			Wait(1);
+
+		}
+		if(Joystick1->GetRawButton(5)){//open close pince
+			etat3=1-etat3;
+			if(etat3==0)
+			{
+
+				verins_3->Set(frc::DoubleSolenoid::kForward);
+
+
+			}
+			else
+			{
+				verins_3->Set(frc::DoubleSolenoid::kReverse);
+
+			}
+			std::cout<<"Verin3"<<std::endl;
+			std::cout<<etat3<<std::endl;
+			Wait(1);
+
+		}
+		if(Joystick1->GetRawButton(6)){//bac
+			etat4=1-etat4;
+			if(etat4==0)
+			{
+				verins_4->Set(frc::DoubleSolenoid::kForward);
+
+			}
+
+
+			{
+				verins_4->Set(frc::DoubleSolenoid::kReverse);
+
+			}
+
+			std::cout<<"Verin4"<<std::endl;
+			std::cout<<etat4<<std::endl;
+			Wait(1);
+		}
+		if(Joystick1->GetRawButton(7)){
+			M5->Set(-0.8);
+
+
+
+		}
+		if(Joystick1->GetRawButton(8)){
+			M5->Set(0);
+
+		}
 
 
 	}
